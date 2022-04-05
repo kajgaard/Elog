cf319889f7d63572a1fc358454474e9e92cc405e
Maria <maria.kajgaard@gmail.com>
2022-04-05 12:51:20 +0200

    Appbar rettet så der ikke er hamburger og instillinger

--- app/src/main/java/com/example/elog/MainActivity.java
+++ app/src/main/java/com/example/elog/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -26,10 +29,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
+
 
         /*binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,8 +54,10 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_oversigt, R.id.nav_afrejse, R.id.nav_fangst, R.id.nav_melding, R.id.nav_landing)
-                .setOpenableLayout(drawer)
                 .build();
+
+
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -58,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -65,12 +73,17 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+     */
+
+    /*
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+     */
 
 
 }
